c62097923055419806f42a6f3e1fa9ee039bc7dd
ang <1731058288@qq.com>
2022-10-13 18:08:31 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -10,9 +10,22 @@
 @Software: PyCharm
 @desc :
 """
-from flask import Flask
+from flask import Flask,url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "Welcome to My Watchlist!"
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='bob'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+    return 'test page'
